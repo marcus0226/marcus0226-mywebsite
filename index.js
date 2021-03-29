6840d45b0c7958915d366e2c131f1357c9ae3018
marcus0226 <bmarcusv15@yahoo.com>
2021-03-28 21:15:03 -0400

    added more js

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ alert("Prepare to be amazed!!!");
 
 const contact = document.getElementsByClassName('contact-info')[0];
 contact.innerText = "bmarcusv15@yahoo.com";
+
+const phone = docoument.getElementsByClassName('phone-number')[0];
+phone.innerText = "Please contact me at 4437226068";
